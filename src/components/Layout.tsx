7e7dc45b4f814944202b04099edcb80234bf9de8
katchinsky <kate-chuprun@gmail.com>
2025-03-24 16:10:31 +0100

    Update theme toggle

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link, Outlet } from 'react-router-dom';
+import { ThemeToggle } from './ThemeToggle';
 
 const Layout: React.FC = () => {
   return (
@@ -22,6 +23,9 @@ const Layout: React.FC = () => {
               </li>
             </ul>
           </nav>
+          <div className="theme-toggle-container">
+            <ThemeToggle />
+          </div>
         </div>
       </header>
       <main className="app-content">
